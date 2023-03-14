166adc07168def3d038994dcac2ec29d0d3afed4
Mohsin <as.mohsin.dev@gmail.com>
2023-03-14 15:21:02 +0500

    clean

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,11 +11,15 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const APIKEY = process.env.APIKEY;
 
 module.exports = {
-  defaultNetwork: "bsc_testnet",
+  defaultNetwork: "goerli_testnet",
   networks: {
     hardhat: {},
     bsc_testnet: {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      accounts: [PRIVATE_KEY],
+    },
+    goerli_testnet: {
+      url: "https://goerli.infura.io/v3/88904a7034a74f0fa8c2de15a4928e34",
       accounts: [PRIVATE_KEY],
     },
   },
